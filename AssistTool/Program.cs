04280ca70c30056b5b99ec9e3005acf2dbcb95d5
lingjinguang <6532315+lingjinguang@user.noreply.gitee.com>
2020-09-01 11:37:49 +0800

    当程序已经打开：结束之前的进程，开启新进程

--- AssistTool/Program.cs
+++ AssistTool/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace PrintAssistTool
 {
@@ -13,6 +14,21 @@ namespace PrintAssistTool
         [STAThread]
         static void Main()
         {
+            #region 当程序已经打开：结束之前的进程，开启新进程
+            //获取与当前进程同名集合（不包括当前进程）
+            List<Process> processes = Process.GetProcesses().Where(p => p.ProcessName == Process.GetCurrentProcess().ProcessName && p.Id != Process.GetCurrentProcess().Id).ToList();
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    process.Kill();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            #endregion
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
